64d67b8d90217f42b982c6f3d1991f15ffb87602
Chris Dale <chrisdale44@gmail.com>
2018-07-27 16:27:41 +0100

    copy orig

--- public/main.js
+++ public/main.js
@@ -2,20 +2,19 @@ const form = document.getElementById('vote-form');
 
 form.addEventListener('submit', e => {
     const choice = document.querySelector('input[name=os]:checked').value;
-    const data = {os: choice};
+    const data = { os: choice };
 
     fetch('http://localhost:3000/poll', {
-        method: 'post',
-        body: JSON.stringify(data),
-        header: new Headers({
-            'Content-Type': 'application/json'
-        })
+      method: 'post',
+      body: JSON.stringify(data),
+      headers: new Headers({
+        'Content-Type': 'application/json'
+      })
     })
-    .then(res => res.json())
-    .then(data => console.log(data))
-    .catch(err => console.log(err));
-
-    e.preventDefault();
+      .then(res => res.json())
+      .then(data => console.log(data))
+      .catch(err => console.log(err));
+      e.preventDefault();
 });
 
 let dataPoints = [
